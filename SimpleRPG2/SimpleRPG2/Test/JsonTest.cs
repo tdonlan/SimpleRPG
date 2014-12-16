f5bd0dc7cb3ad4782f316c7b3aae15a2ff87f02a
Tim Donlan <timdonlan@gmail.com>
2014-12-16 16:54:10 -0500

    Added Json.net, testing reading/writing data objects

---
+++ SimpleRPG2/SimpleRPG2/Test/JsonTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Newtonsoft.Json;
+
+namespace SimpleRPG2
+{
+    public class JsonTest
+    {
+        public static void CreateJson()
+        {
+            Ability fireball= AbilityFactory.getFireball();
+            string jsonStr= JsonConvert.SerializeObject(fireball);
+
+
+        }
+
+        public static void ReadJson()
+        {
+           
+
+            string jsonStr = File.ReadAllText(@"C:\GameDev\Dev\SimpleRPG2\SimpleRPG2\DataFiles\Abilities.json");
+
+
+
+            Ability fireball = JsonConvert.DeserializeObject<Ability>(jsonStr);
+
+
+            
+        }
+    }
+}
